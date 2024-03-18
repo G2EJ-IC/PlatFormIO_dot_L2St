5b2083db8d903f98b89ca506d01a5c0144c2728b
G2EJ-IC <IC.G2EJ@outlook.com>
2024-03-18 07:25:31 -0500

    Bk - 2024.03.18 07:25.
    Bk - 2024.03.18 07:25.

--- Ej_EEZ/Ej-EEZ_LVGLv8_3_11/lib/ui/actions.cpp
+++ Ej_EEZ/Ej-EEZ_LVGLv8_3_11/lib/ui/actions.cpp
@@ -8,23 +8,25 @@
 
 extern void action_fn_conectar_wi_fi(lv_event_t * e)
 {
-	lv_obj_add_flag(objects.ui_panel_conectar_wi_fi, LV_OBJ_FLAG_HIDDEN);
+	// lv_obj_add_flag(objects.ui_panel_conectar_wi_fi, LV_OBJ_FLAG_HIDDEN);
+	lv_label_set_text(objects.ui_label_conectar_wi_fi, "Conecte el celular a la red \n(.:: AIoT_NodeESP32 ::.)\n y configure WiFi....");
+	lv_obj_clear_flag(objects.ui_panel_conectar_wi_fi, LV_OBJ_FLAG_HIDDEN);	
 	ConectarWiFi_AIoT();
 	if (WiFi.status() == WL_CONNECTED) {
 	    lv_label_set_text(objects.ui_lab_ssid, WiFi.SSID().c_str());
 		lv_label_set_text(objects.ui_lab_ip, WiFi.localIP().toString().c_str());
 		lv_label_set_text(objects.ui_lab_dns, WiFi.dnsIP().toString().c_str());
 		lv_label_set_text(objects.ui_lab_mac, WiFi.macAddress().c_str());
-		lv_label_set_text(objects.ui_label_conectar_wi_fi, "Conecte el celular a la red \n(.:: AIoT_NodeESP32 ::.)\n y configure WiFi....");
-		lv_obj_clear_flag(objects.ui_panel_conectar_wi_fi, LV_OBJ_FLAG_HIDDEN);	
+		// lv_label_set_text(objects.ui_label_conectar_wi_fi, "Conecte el celular a la red \n(.:: AIoT_NodeESP32 ::.)\n y configure WiFi....");
+		// lv_obj_clear_flag(objects.ui_panel_conectar_wi_fi, LV_OBJ_FLAG_HIDDEN);	
 		lv_obj_set_style_bg_color(objects.ui_bt_conectado, lv_color_hex(0x008000), LV_PART_MAIN | LV_STATE_DEFAULT);
 	} else {
 	    lv_label_set_text(objects.ui_lab_ssid, "xx.xx.xx.xx");
 		lv_label_set_text(objects.ui_lab_ip, "xx.xx.xx.xx");
 		lv_label_set_text(objects.ui_lab_dns, "xx.xx.xx.xx");
 		lv_label_set_text(objects.ui_lab_mac, "xx:xx:xx:xx:xx:xx");
-		lv_label_set_text(objects.ui_label_conectar_wi_fi, "Conecte el celular a la RED \n(.:: AIoT_NodeESP32 ::.)\n y configure WiFi....");
-		lv_obj_clear_flag(objects.ui_panel_conectar_wi_fi, LV_OBJ_FLAG_HIDDEN);
+		// lv_label_set_text(objects.ui_label_conectar_wi_fi, "Conecte el celular a la RED \n(.:: AIoT_NodeESP32 ::.)\n y configure WiFi....");
+		// lv_obj_clear_flag(objects.ui_panel_conectar_wi_fi, LV_OBJ_FLAG_HIDDEN);
 		lv_obj_set_style_bg_color(objects.ui_bt_conectado, lv_color_hex(0xFF0000), LV_PART_MAIN | LV_STATE_DEFAULT);
 	}
 }
