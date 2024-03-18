508ee8dead061bd8d7d0a84d99858a3b98c512b4
G2EJ-IC <IC.G2EJ@outlook.com>
2024-03-17 21:02:44 -0500

    Bk - 2024.03.17 21:01.
    Bk - 2024.03.17 21:01.

--- Ej_EEZ/Ej-EEZ_LVGLv8_3_11/lib/ui/actions.h
+++ Ej_EEZ/Ej-EEZ_LVGLv8_3_11/lib/ui/actions.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+#include <lvgl.h>
+
 extern void action_fn_conectar_wi_fi(lv_event_t * e);
 extern void action_fn_des_conectar_wi_fi(lv_event_t * e);
 extern void action_fn_presionar_bt_ok(lv_event_t * e);
